c8c00520f4dbafeae723aa05b2e85eb065020da6
sksangam <subhashsangam@gmail.com>
2023-06-12 07:24:17 +0530

    added leaflet map

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,32 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+const mapZoomLevel = 13;
+
+const _getCurrentLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+        _loadMap([latitude, longitude]);
+      },
+      error => {
+        console.log(error.message);
+      }
+    );
+  }
+};
+
+const _loadMap = coords => {
+  var map = L.map('map').setView(coords, mapZoomLevel);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  L.marker(coords).addTo(map).bindPopup('marker').openPopup();
+};
+
+_getCurrentLocation();
